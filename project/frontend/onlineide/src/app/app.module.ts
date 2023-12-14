9c6b45ca8ddc32e07dd1a54950da0aac3920283e
burhanyesiloz <burhan.yesiloez@stud.uni-due.de>
2023-12-14 11:54:30 +0100

    add materialUI

--- project/frontend/onlineide/src/app/app.module.ts
+++ project/frontend/onlineide/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -43,7 +44,7 @@ import { CiCdService } from './services/ci-cd.service';
     CodeEditorService,
     CiCdService,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
