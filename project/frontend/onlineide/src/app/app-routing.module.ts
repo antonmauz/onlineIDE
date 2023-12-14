daa2217ba6e3ace75092e34e2071f193d03fe5d6
burhanyesiloz <burhan.yesiloez@stud.uni-due.de>
2023-12-14 11:37:14 +0100

    add routes with paths

--- project/frontend/onlineide/src/app/app-routing.module.ts
+++ project/frontend/onlineide/src/app/app-routing.module.ts
@@ -1,18 +1,23 @@
-// app-routing.module.ts
+// src/app/app-routing.module.ts
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import { HomeComponent } from './home/home.component';
-import { EditorComponent } from './editor/editor.component';
-import { SettingsComponent } from './settings/settings.component';
+import { UserAuthenticationComponent } from './user-authentication/user-authentication.component';
+import { AuthorizationComponent } from './authorization/authorization.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectDetailsComponent } from './projects/project-details/project-details.component';
 import { CodeEditorComponent } from './code-editor/code-editor.component';
+import { CiCdStatusComponent } from './ci-cd-status/ci-cd-status.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'editor', component: EditorComponent },
-  { path: 'settings', component: SettingsComponent },
+  { path: 'login', component: UserAuthenticationComponent },
+  { path: 'authorization', component: AuthorizationComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'projectdetails', component: ProjectDetailsComponent },
   { path: 'codeeditor', component: CodeEditorComponent },
+  { path: 'cicdstatus', component: CiCdStatusComponent },
+
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default route
 ];
 
 @NgModule({
