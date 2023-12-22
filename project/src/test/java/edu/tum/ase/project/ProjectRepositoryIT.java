2093e63af36c8afc09afdaa9a20abaea1e78a75b
antonmauz <anton.mauz@gmx.de>
2023-12-22 14:40:23 +0100

    Fix in application.yaml and first integration test

---
+++ project/src/test/java/edu/tum/ase/project/ProjectRepositoryIT.java
@@ -0,0 +1,42 @@
+package edu.tum.ase.project.repository;
+
+import edu.tum.ase.project.model.Project;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class ProjectRepositoryIT {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @BeforeEach
+    void setUp() {
+        projectRepository.deleteAll();
+    }
+
+    @Test
+    void should_ReturnProject_When_QueriedByName() {
+        // Given
+        String projectName = "Test Project";
+        Project savedProject = new Project(projectName);
+        projectRepository.save(savedProject);
+
+        // When
+        Project foundProject = projectRepository.findByName(projectName);
+
+        // Then
+        assertThat(foundProject).isNotNull();
+        assertThat(foundProject.getName()).isEqualTo(projectName);
+    }
+}
