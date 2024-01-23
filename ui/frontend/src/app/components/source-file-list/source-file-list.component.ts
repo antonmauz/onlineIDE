07c410d4190c320be75c42d33c411dd880866c35
burhanyesiloz <burhan.yesiloez@stud.uni-due.de>
2024-01-23 10:41:35 +0100

    add source file list component

---
+++ ui/frontend/src/app/components/source-file-list/source-file-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+@Component({
+  selector: 'app-source-file-list',
+  standalone: true,
+  imports: [MatListModule, MatButtonModule],
+  templateUrl: './source-file-list.component.html',
+  styleUrl: './source-file-list.component.css'
+})
+export class SourceFileListComponent {
+
+}
