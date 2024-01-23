36250ef919f032962016f62f5b158266be0dd3da
burhanyesiloz <burhan.yesiloez@stud.uni-due.de>
2024-01-23 10:01:39 +0100

    refactoring

--- project/frontend/onlineide/src/app/user-authentication/user-authentication.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { UserAuthenticationService } from '../services/user-authentication.service';
-
-@Component({
-  selector: 'app-user-authentication',
-  templateUrl: './user-authentication.component.html',
-  styleUrls: ['./user-authentication.component.css'],
-})
-export class UserAuthenticationComponent {}
