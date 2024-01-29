a85aa9ced708e986712618469a80c9d7d00a697e
cdans <ch.dansard@gmail.com>
2024-01-29 09:30:19 +0100

    Add rough login component and route

--- ui/frontend/src/app/app.routes.ts
+++ ui/frontend/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 
 import { EditorComponent } from './components/editor/editor.component';
+import { LoginComponent } from './components/login/login.component';
 import { ProjectDetailsComponent } from './components/project-details/project-details.component';
 import { ProjectListComponent } from './components/project-list/project-list.component';
 
 export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
   {
     path: 'projects/:id',
     component: ProjectDetailsComponent,
