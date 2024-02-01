78cf76f5696a5a9367e8dd1922b3104707ce3977
cdans <ch.dansard@gmail.com>
2024-02-01 15:32:17 +0100

    Add rest controller and security configuration

--- ui/src/main/java/com/example/ui/UiApplication.java
+++ ui/src/main/java/com/example/ui/UiApplication.java
@@ -6,28 +6,33 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
+@RestController
 public class UiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UiApplication.class, args);
+    }
 
-	@Configuration
-	public class WebMvcConfiguration implements WebMvcConfigurer {
-		@Override
-		public void addResourceHandlers(ResourceHandlerRegistry registry) {
-			// Serve static resources
-			registry.addResourceHandler("/static/browser/**")
-					.addResourceLocations("classpath:/static/browser/");
-		}
+    @Configuration
+    public class WebMvcConfiguration implements WebMvcConfigurer {
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            // Serve static resources
+            registry.addResourceHandler("/static/browser/**")
+                    .addResourceLocations("classpath:/static/browser/");
+        }
 
-	}
+    }
 
     @Controller
     public class ForwardToIndexController {
@@ -36,5 +41,16 @@ public class UiApplication {
         public String forward(HttpServletRequest request) {
             return "forward:/index.html";
         }
+    }
+
+    @GetMapping("/api/authenticated")
+    public boolean authenticated() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        if (authentication != null) {
+            return authentication.getAuthorities().stream()
+                    .noneMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_ANONYMOUS"));
+        }
+        return false;
     }
 }
