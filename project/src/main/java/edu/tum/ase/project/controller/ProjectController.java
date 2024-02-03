509dbb75e8fea734b5188e0de68842602076ab00
cdans <ch.dansard@gmail.com>
2024-02-03 14:47:44 +0100

    Remove unused imports in ProjectController

--- project/src/main/java/edu/tum/ase/project/controller/ProjectController.java
+++ project/src/main/java/edu/tum/ase/project/controller/ProjectController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 import edu.tum.ase.project.model.Project;
 import edu.tum.ase.project.model.ShareProjectDTO;
