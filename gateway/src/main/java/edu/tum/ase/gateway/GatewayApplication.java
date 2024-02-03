34b01c8b3c516ff807bb6acad959fadf1c7ff67e
cdans <ch.dansard@gmail.com>
2024-02-03 18:54:30 +0100

    Add custom route locator for gateway

--- gateway/src/main/java/edu/tum/ase/gateway/GatewayApplication.java
+++ gateway/src/main/java/edu/tum/ase/gateway/GatewayApplication.java
@@ -2,9 +2,21 @@ package edu.tum.ase.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("project-service", r -> r.path("/project/**")
+						.uri("lb://project-service"))
+				.route("ui-service", r -> r.path("/ui/**")
+						.uri("lb://ui-service"))
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
