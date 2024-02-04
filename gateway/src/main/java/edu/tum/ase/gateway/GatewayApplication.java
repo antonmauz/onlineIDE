d2a24bd16f20b784c269430acabc8c08a6302fe2
cdans <ch.dansard@gmail.com>
2024-02-04 19:50:03 +0100

    TEST: Update dependencies and routes configuration

--- gateway/src/main/java/edu/tum/ase/gateway/GatewayApplication.java
+++ gateway/src/main/java/edu/tum/ase/gateway/GatewayApplication.java
@@ -1,45 +1,12 @@
 package edu.tum.ase.gateway;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class GatewayApplication {
-	@Bean
-	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route("project-service", r -> r.path("/project/**")
-						.filters(f -> f.rewritePath("/project/(?<segment>.*)", "/${segment}"))
-						.uri("lb://project-service"))
-				.route("ui-service", r -> r.path("/**")
-						.filters(f -> f.rewritePath("/ui/(?<segment>.*)", "/${segment}"))
-						.uri("lb://ui-service"))
-				.build();
-	}
-
-	@Bean
-	public CorsWebFilter corsWebFilter() {
-		CorsConfiguration corsConfig = new CorsConfiguration();
-		corsConfig.setAllowedOrigins(Arrays.asList("*")); // Or specify your frontend's origin
-		corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-		corsConfig.setAllowedHeaders(Arrays.asList("*"));
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", corsConfig);
-
-		return new CorsWebFilter(source);
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
-
 }
